0e6635e98d4019fc3e1318bbf6201aaa342092ed
Towel Bunny <86595431+TowelBulletBunny@users.noreply.github.com>
2023-11-26 10:34:37 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// JavaScript code for the demo game website
+
+// Add an event listener for the "Play Now" buttons
+const playButtons = document.querySelectorAll('.game button');
+playButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    alert('Sorry, this is just a demo.');
+    });
+});
